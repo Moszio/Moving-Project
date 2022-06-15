fa07f31f1aceb0ce76da63cc73039b8441a5c4c0
andor <latandor@gmail.com>
2022-06-15 12:20:53 -0400

    style.css modification

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 const myBtnLeft = document.getElementById("move-left");
+const myBtnRight = document.getElementById("move-right");
+const smlSq = document.getElementById("small-square");
 
-myBtnLeft.addEventListener("keydown", function(e){
- const left = 
- console.log("I was clicked!")
+
+function arrowLeft() {
+  const moveLeft = smlSq.style.left.replace("px", "")
+  const left = parseInt(moveLeft, 10)
+
+  if(left > 0) {
+    smlSq.style.left = `${left - 6}px`
+  }
+}
+
+function arrowRight() {
+  const moveRight = smlSq.style.left.replace("px", "")
+  const right = parseInt(moveRight, 10)
+
+  if(right < 360) {
+    smlSq.style.left = `${right + 6}px`
+  }
+}
+
+myBtnLeft.addEventListener("click", function(e){
+arrowLeft();
+})
+
+myBtnRight.addEventListener("click", function(e){
+arrowRight();
+})
+
+
+document.addEventListener("keydown", function(event){
+  if(event.key === "ArrowLeft")
+  arrowLeft();
+  else if (event.key === "ArrowRight")
+  arrowRight();
 })
